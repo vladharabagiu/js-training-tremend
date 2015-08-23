3d4d89c18c2e2d8557cb685f76b5db223826b3b8
Razvan Tache <tache.razvan@webmonsters.ro>
2015-08-23 21:08:17 +0300

    Add the solution to Checkout app.

--- Exercises/Checkout/scripts/script.js
+++ Exercises/Checkout/scripts/script.js
@@ -8,3 +8,50 @@
  */
 
 var final_prices = [];
+
+// iterate over each order
+for (var i = 0; i < orders.length; i++) {
+
+    // start the sum as 0
+    var sum = 0.0;
+
+    // iterate over the order items
+    for (var j = 0; j < orders[i].items.length; j++) {
+
+        // remember the current item
+        var item = orders[i].items[j];
+
+        // declare the cost of the item
+        var cost = 0;
+
+        // get the item cost by iterating over the items array and find the matching item
+        for (var k = 0; k < items.length; k++) {
+            if (items[k].code === item.item) {
+                cost = items[k].cost;
+                break;
+            }
+        }
+
+        // add the result to the sum of the item
+        sum += item.quantity * cost;
+    }
+
+    // check if the order has an employee field
+    if ((typeof orders[i].employee) !== "undefined") {
+
+        // find the employee by iterating over employees
+        for (j = 0; j < employees.length; j++) {
+            if (employees[j].id === orders[i].employee) {
+                // check and apply the discount
+                if (employees[j].discount >= 0 && employees[j].discount <=70) {
+                    sum *= 1 - employees[j].discount/100;
+                } else {
+                    sum = false;
+                }
+            }
+        }
+    }
+
+    // add the element to final_prices
+    final_prices.push({order_id: orders[i].id, cost: sum});
+}
