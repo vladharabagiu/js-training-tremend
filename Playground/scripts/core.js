64bac9ea46d69de0506eae079c778c0f67263a21
Razvan Tache <tache.razvan@webmonsters.ro>
2015-10-11 19:38:15 +0300

    Fix small error with circular refferences

--- Playground/scripts/core.js
+++ Playground/scripts/core.js
@@ -5,7 +5,12 @@
     console.log = function (message) {
         var body = document.querySelector("body");
         var p = document.createElement("p");
-        p.textContent = (message !== undefined) ? JSON.stringify(message) : "undefined"
+
+        try {
+            p.textContent = (message !== undefined) ? JSON.stringify(message) : "undefined"
+        } catch (exception) {
+            oldLog.apply(console, arguments);
+        }
         if (typeof message == "function") {
             p.textContent = message;
         }
